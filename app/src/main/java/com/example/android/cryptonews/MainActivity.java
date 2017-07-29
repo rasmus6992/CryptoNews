343f3bdb6470a1356c53dc0546a47afb3f4d0b8f
ayuub2 <ayuubadan@outlook.com>
2017-07-30 00:12:28 +0100

    Added volley code

--- app/src/main/java/com/example/android/cryptonews/MainActivity.java
+++ app/src/main/java/com/example/android/cryptonews/MainActivity.java
@@ -3,11 +3,34 @@ package com.example.android.cryptonews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.coinmarketcap.com/v1/ticker/";
+        JsonObjectRequest jsObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
     }
 }
