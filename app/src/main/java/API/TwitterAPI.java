c98a509e5af4745c85d3e8ad171995f23843a53a
ayuub2 <ayuubadan@outlook.com>
2017-07-31 15:37:59 +0100

    Removed api classes since they were no longer needed.
    Retrieved data from CoinMarketAPI successfully

--- app/src/main/java/API/TwitterAPI.java
+++
@@ -1,7 +0,0 @@
-package API;
-
-/**
- * Created by ayuub2 on 28/07/2017.
- */
-public class TwitterAPI {
-}
--- app/src/main/java/com/example/android/cryptonews/MainActivity.java
+++ app/src/main/java/com/example/android/cryptonews/MainActivity.java
@@ -2,35 +2,62 @@ package com.example.android.cryptonews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public ArrayList<JSONObject> coinData;  // holds each coins information
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Using volley to add requests to a queue
         RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(coinMarketData());
+    }
+
+    //This method returns a  request to CoinMarket API to be added to the queue.
+    public Request coinMarketData(){
         String url = "https://api.coinmarketcap.com/v1/ticker/";
-        JsonObjectRequest jsObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
-
+            public void onResponse(JSONArray response) {
+                try {
+                    coinData = new ArrayList<>();
+                    for(int i = 0; i < response.length(); i++){
+                        coinData.add(response.getJSONObject(i));
+                    }
+                    Log.d("CoinMarketAPI", coinData.toString());
+                }
+                catch (JSONException e){
+                    Log.d("CoinMarketAPI",e.toString());
+                }
             }
-
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Log.d("CoinMarketAPI",error.toString());
             }
         });
+
+        return jsArrayRequest;
     }
+
+
 }
